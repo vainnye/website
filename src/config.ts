9bd528aff68d67d5966dc8d6794a628860d01632
vainnye <vianney.jcqmt@gmail.com>
2025-12-25 09:08:48 -0500

    adjusted content and translations

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,8 @@
 export type Lng = (typeof config.lngs)[number];
 
+// TODO: use a i18n library to not only store strings but also more complex data structures
+// For example, you might want to store the display duration for each anim_line_X to be used in the morph animation
+
 export const config = {
   lngs: ["fr", "en"] as const,
   default_lng: "en" as const,
@@ -17,20 +20,20 @@ export const config = {
       en: "passer en français",
     },
     anim_line_1: {
-      en: "Hi I'm Vianney!",
+      en: "Hi, I'm Vianney!",
       fr: "Bonjour !",
     },
     anim_line_2: {
-      en: 'call me "V N A"',
+      en: "pronounced V-N-A",
       fr: "moi c'est Vianney",
     },
     anim_line_3: {
-      en: "hiring an intern?",
-      fr: "recherche stage",
+      en: "Looking for an intern?",
+      fr: "Besoin d'un stagiaire ?",
     },
     anim_line_4: {
-      en: "reach me out!",
-      fr: "contactez moi !",
+      en: "Let's get in touch!",
+      fr: "Contactez-moi !",
     },
     anim_line_5: {
       en: "Vianney",
@@ -41,8 +44,8 @@ export const config = {
       fr: "actuellement à Chicoutimi (Québec)",
     },
     bio: {
-      en: `I'm a 20-year-old **backend developer** and **data scientist** from France. Currently pursuing a double bachelor's degree with [Paris-Saclay University](https://www.universite-paris-saclay.fr/en) and [Chicoutimi University (UQAC)](https://www.uqac.ca/).`,
-      fr: `Bonjour ! Je suis **développeur backend** et **data scientist** français. Je poursuis actuellement un double diplôme BUT + Baccalauréat Québécois avec [l'Université Paris-Saclay](https://www.universite-paris-saclay.fr/) et [l'Université du Québec à Chicoutimi (UQAC)](https://www.uqac.ca/).`,
+      en: `I'm a 20-year-old **full-stack developer** and **data scientist** from France. Currently pursuing a double bachelor's degree with [Paris-Saclay University](https://www.universite-paris-saclay.fr/en) and the [University of Quebec at Chicoutimi (UQAC)](https://www.uqac.ca/).`,
+      fr: `Je suis un **développeur full-stack** et **data scientist** de 20 ans, originaire de France. Je prépare actuellement un double diplôme (BUT & Bachelor) avec [l'Université Paris-Saclay](https://www.universite-paris-saclay.fr/) et [l'Université du Québec à Chicoutimi (UQAC)](https://www.uqac.ca/).`,
     },
     url_linkedin: {
       en: "https://www.linkedin.com/in/vianney-jacquemot/?locale=en_US",
